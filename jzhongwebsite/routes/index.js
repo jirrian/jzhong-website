5db20db9447162e3053585626ce3f00b2f166f10
Jillian <jillianzhong@yahoo.com>
2016-10-01 01:46:17 -0400

    set up routes, basic layout, stylesheets, added content to apps and misc media pages

--- jzhongwebsite/routes/index.js
+++ jzhongwebsite/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+//home page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'About', stuff:'Jillian Zhong is a programmer, artist, and content creator based in New York and Philly.' });
+});
+
+//contact page
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact', stuff:'jillianzhong@nyu.edu' });
+});
+
+router.get('/apps', function(req, res, next) {
+  res.render('apps', { title: 'Mobile Apps' });
+});
+
+router.get('/games', function(req, res, next) {
+  res.render('games', { title: 'Games' });
+});
+
+router.get('/miscmedia', function(req, res, next) {
+  res.render('miscmedia', { title: 'Miscellaneous Digital Media' });
+});
+
+router.get('/photography', function(req, res, next) {
+  res.render('index', { title: 'Photography' });
 });
 
 module.exports = router;
