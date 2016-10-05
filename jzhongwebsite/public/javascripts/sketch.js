5039e4b8118dff89e639d779af6a1d8bfbc0aa53
Jillian <jillianzhong@yahoo.com>
2016-10-04 21:53:40 -0400

    implemented sketch canvas that covers entire window

--- jzhongwebsite/public/javascripts/sketch.js
+++ jzhongwebsite/public/javascripts/sketch.js
@@ -1,10 +1,12 @@
 function setup() {
-	createCanvas(600, 400);
-    
+	var sketchCanvas = createCanvas(window.innerWidth, window.innerHeight);
+    sketchCanvas.parent('sketch');
 
 }
 
 function draw() {
-	
+	//sketchCanvas.canvas.width = window.innerWidth;
+	//sketchCanvas.canvas.height = window.innerHeight;
+	background(209);
   	ellipse(50, 50, 80, 80);
 }
